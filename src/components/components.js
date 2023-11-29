e700de7ada0062f307fcdf0e63cccd579659dd8e
corinachistol <chistolcorina19@gmail.com>
2023-11-29 15:54:47 +0200

    conditional textrendering added

--- src/components/components.js
+++ src/components/components.js
@@ -1,3 +1,5 @@
+import { skills } from "./skills"
+console.log(skills)
 
 function Avatar( {source, name} ) {
     return (
@@ -17,24 +19,33 @@ function Intro( {name,description} ) {
   )
 }
 
-function Skill ({color,skills,emoji}) {
+function Skill ({color,skills,level}) {
   return (
     <div style={{backgroundColor:color}} className="skill">
       <span>{skills}</span>
-      <span>{emoji}</span>
+      <span>
+        {level ==="beginner" && "🧑"}
+        {level ==="intermediate" && "👍"}
+        {level ==="advanced" && "💪"}
+      </span>
     </div>
   )
 }
 
 function SkillList() {
+    
   return (
     <div className="skill-list">
-      <Skill color="lightblue" skills="HTML+CSS" emoji="🖔" />
-      <Skill color="yellow" skills="Javascript" emoji="🖔" />
+      {skills.map(skill=>(
+        <Skill color={skill.color} skills={skill.skill} level={skill.level} />
+      ))}
+
+      {/* <Skill color="lightblue" skills="HTML+CSS" emoji="👍" />
+      <Skill color="yellow" skills="Javascript" emoji="🧑" />
       <Skill color="green" skills="Web Design" emoji="🖔" />
       <Skill color="red" skills="Git and GitHub" emoji="🖔" />
       <Skill color="darkblue" skills="React" emoji="🖔" />
-      <Skill color="orange" skills="Svelte" emoji="🖔" />
+      <Skill color="orange" skills="Svelte" emoji="🖔" /> */}
     </div>
   )
 }
---
+++ src/components/skills.js
@@ -0,0 +1,32 @@
+export const skills = [
+    {
+        skill: "HTML+CSS",
+        level: "advanced",
+        color:"#2662EA"
+    },
+    {
+        skill: "Javascript",
+        level: "advanced",
+        color:"#EFD81D"
+    },
+    {
+        skill: "Web Design",
+        level: "advanced",
+        color:"#C3DCAF"
+    },
+    {
+        skill: "Git and GitHub",
+        level: "intermediate",
+        color:"#E84F33"
+    },
+    {
+        skill: "React",
+        level: "advanced",
+        color:"60DAF8"
+    },
+    {
+        skill: "Svelte",
+        level: "beginner",
+        color:"#FF3B00"
+    },
+]
